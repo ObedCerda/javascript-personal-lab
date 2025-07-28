0f3266a4e625b54543c200a65c684c24eafda93b
Obed Cerda <obed.cerda.dev@gmail.com>
2025-07-27 23:24:21 -0600

    Se declara valor ingresado por el usuario y se muestra en la consola

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
 // Este alert es para notificar en el navegador
-alert('Hello World!');
+//alert('Hello World!');
+// prompt se utiliza para pedir información al usuario
+let numeroUsuario = prompt("Ingresa tu numero");
+//alert ("Tu numero es: " + numeroUsuario);
+console.log("El numero ingresado por el usuario es: " + numeroUsuario);
