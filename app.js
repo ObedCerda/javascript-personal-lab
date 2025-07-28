6a42dd2e027ed2edebb98d38bdbb70e85db502f2
Obed Cerda <obed.cerda.dev@gmail.com>
2025-07-28 00:38:46 -0600

    se agrega archivo desafio.js que es un archivo con los desafios de la actividad, el html lo tiene como src y un typo en app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 let numeroUsuario = prompt("Ingresa tu numero");
-let numeroSecreto = 7
+let numeroSecreto = 7;
 
 console.log("El numero ingresado por el usuario es: " + numeroUsuario);
 if (numeroUsuario == numeroSecreto)
---
+++ desafio.js
@@ -0,0 +1,31 @@
+// desafios de logica
+//Muestra una alerta con el mensaje "¿Bienvenido a nuestro sitio web"
+alert("Bienvenido a nuestro sitio web");
+// Declara una variable llamada nombre y asignale el valor "Lua"
+let nombre = "Lua";
+// Crear una variable llamada edad y asignale el valor 25
+let edad = 25;
+// Establece una variable numeroDeVentas y asígnale el valor 50.
+let numeroDeVentas = 50;
+// Establece una variable saldoDisponible y asígnale el valor 1000.
+let saldoDisponible = 1000;
+console.log("Edad: " + edad);
+console.log("Nombre: " + nombre);
+console.log("Numero de ventas: " + numeroDeVentas);
+console.log("Saldo disponible: " + saldoDisponible);
+
+// Muestra una alerta con el texto "¡Error! Completa todos los campos".
+alert("Error completa los campos");
+// Declara una variable llamada mensajeDeError y asígnale el valor "¡Error! Completa todos los campos".
+//  Ahora muestra una alerta con el valor de la variable mensajeDeError .
+let mensajeDeError = "Error completa los campos";
+alert(mensajeDeError);
+// Utiliza un prompt para preguntar el nombre del usuario y almacénalo en la variable nombre.
+let nombreIngresado = prompt("Ingresa tu nombre");
+// Pide al usuario que ingrese su edad usando un prompt y almacénala en la variable edad.
+let edadIngresada = prompt("Ingresa tu edad");
+//  Ahora, si la edad es mayor o igual a 18, muestra una alerta con el mensaje
+//  "¡Puedes obtener tu licencia de conducir!"
+if (edadIngresada >= 18){
+    alert("Puedes obetener tu licencia de conducir");
+}
