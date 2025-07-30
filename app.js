8f13f8c35e1db5ffdac391acd943d9a02b8ab995
Obed Cerda <obed.cerda.dev@gmail.com>
2025-07-29 23:38:01 -0600

    Se quita el src de desafio.js y se agrega un else a la app.js cuando no acierta el numero

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 
-let numeroUsuario = prompt("Ingresa tu numero");
 let numeroSecreto = 7;
+let numeroUsuario = prompt("Ingresa tu numero entre 1 y 10");
+
 
 console.log("El numero ingresado por el usuario es: " + numeroUsuario);
-if (numeroUsuario == numeroSecreto)
+
+if (numeroUsuario == numeroSecreto) // Compara el numero ingresado por el usuario con el numero secreto
 {
     console.log("Felicidades, has acertado el numero secreto");
+}
+
+else 
+{
+    console.log("Lo siento, el numero secreto era: " + numeroSecreto);
 }
