24a5067878f04edbe09720a4f5432dfdeba044d1
Obed Cerda <obed.cerda.dev@gmail.com>
2025-07-31 01:10:09 -0600

    ejercicio 02, typo

--- desafio.js
+++ desafio.js
@@ -68,19 +68,7 @@ else
 }
 
 
-/*03
-Crea un sistema de puntuación para un juego. Si la puntuación es mayor o igual a 100, muestra "¡Felicidades, has ganado!". 
-En caso contrario, muestra "Intentalo nuevamente para ganar.".
-let puntuacion = prompt("Ingresa tu puntuación");
-if (puntuacion >= 100)
-{
-    alert("Felicidades, has ganado");
-}
-else
-{
-    alert("intentalo nuevamente para ganar");
-}
-*/
+
 
 
 
